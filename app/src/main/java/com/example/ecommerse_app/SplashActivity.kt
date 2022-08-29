d352a2a342db5191cf7f25467ba5039417d76198
userName <quaviciouscozmonito56@gmail.com>
2022-08-29 13:17:39 +0300

    first commit

--- app/src/main/java/com/example/ecommerse_app/SplashActivity.kt
+++ app/src/main/java/com/example/ecommerse_app/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.example.ecommerse_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
