e1663a639c467e8397acc80429882f9a63125909
userName <quaviciouscozmonito56@gmail.com>
2022-08-30 14:19:23 +0300

    btn intent

--- app/src/main/java/com/example/ecommerse_app/SplashActivity.kt
+++ app/src/main/java/com/example/ecommerse_app/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ecommerse_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        //Button with intent
+
+        val btn = findViewById<Button>(R.id.btn_intent)
+
+        btn.setOnClickListener {
+           val try_intent = Intent( this,HomeActivity::class.java)
+            startActivity(try_intent)
+        }
     }
 }
